9dee169d8563a236474d97c98d4d09de044ce6a5
mohamed khatab <hamosuper8@gmail.com>
2017-09-26 20:21:16 +0200

    Add files via upload

---
+++ مشروع الاول/js/app.js
@@ -0,0 +1,12 @@
+
+function adds() {
+    var li = document.createElement("LI");
+document.getElementById("ssss").appendChild(li);
+li.innerHTML=document.getElementById("textt").value;
+};
+
+function remove() {
+    
+    document.getElementById("ssss").innerHTML="";
+};
+
